98df054c1f8087bb19249778a998e02d22dc3e29
JosephObote <joeobote01@gmail.com>
2023-09-19 22:19:29 +0300

    attempeted assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
+#APPROVED_BREEDS = [
+    #"Chihuahua",
+    #"Corgi",
+    #"Shar Pei",
+    #"Beagle",
+    #"French Bulldog",
+    #"Pug",
+    #"Pointer"
+#]
 
 class Dog:
-    pass
+    species= "Homosapien"
+    def __init__(self, name="joe", age=28, email="joeobote01@mail.com",height=190):
+        self.name= name
+        self.age=age
+        self.email=email
+        self.height= height
+
+        william =Dog(name="Abwao", )
+    print("william")
+    
